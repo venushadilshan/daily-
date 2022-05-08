5b798d1616acc2e4048dcb27dcef7cdae17c6788
Venusha <venusha125@gmail.com>
2022-05-08 16:10:24 +0530

    Cards

--- src/App.js
+++ src/App.js
@@ -8,8 +8,12 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-
-
+import ListCard from './components/ListCard';
+import { Stack } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import { Grid } from '@mui/material';
 
 
 function App() {
@@ -17,39 +21,67 @@ function App() {
   const [dailyData, setDailyData] = useState()
 
   useEffect(() => {
-   
+
     if (localStorage.getItem("dailyData") === null) {
-      localStorage.setItem("dailyData",JSON.stringify(dailyDataTemp))
+      localStorage.setItem("dailyData", JSON.stringify(dailyDataTemp))
     }
 
-    else
-    {
-      var temp =  localStorage.getItem("dailyData")
-    
-    setDailyData(JSON.parse(temp))
-    console.log(dailyData)
+    else {
+      var temp = localStorage.getItem("dailyData")
 
-      
+      setDailyData(JSON.parse(temp))
+      console.log(dailyData)
+
+
     }
 
-    
+
   }, [])
   return (
     <div >
 
-<Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-         
-            
-         
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Daily
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Daily
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Grid container >
+        <Grid item xs={100} md={4} lg={3}>
+          <Card variant="outlined" sx={{backgroundColor:"info.main", margin:"1em"}}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+               Current Balance
+              </Typography>
+              <Typography variant="h5" component="div" sx={{color:"white"}}>
+                153873.56 LKR
+              </Typography>
+             
+            </CardContent>
+          
+          </Card>
+        </Grid>
+      </Grid>
+
+      <Stack
+        direction="row"
+        justifyContent="center"
+        alignItems="flex-start"
+        flexWrap="wrap"
+        lg={{justifyContent:"flex-start"}}
+      >
+        <ListCard />
+        <ListCard />
+        <ListCard />
+        <ListCard />
+        <ListCard /> <ListCard /> <ListCard /> <ListCard />
+      </Stack>
+
+
     </div>
   );
 }
---
+++ src/components/ListCard.js
@@ -0,0 +1,31 @@
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+export default function ListCard() {
+    return (
+        <div className='card'>
+            <Card variant="outlined" >
+                <CardContent>
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        Word of the Day
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                        hgjghj
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        adjective
+                    </Typography>
+                    <Typography variant="body2">
+                        well meaning and kindly.
+                        <br />
+                        {'"a benevolent smile"'}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Learn More</Button>
+                </CardActions></Card>
+        </div>)
+}
