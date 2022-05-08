bd38065a0b43c9c9143e9e916524a9d24a75628f
Venusha Dilshan Dushmantha <venusha125@gmail.com>
2022-05-08 14:02:31 +0530

    add MUI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import dailyDataTemp from './SampleData'
 import * as Mui from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+
 
 
 function App() {
@@ -30,7 +37,19 @@ function App() {
   return (
     <div >
 
-     <Mui.Button variant="contained">Test</Mui.Button>
+<Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+         
+            
+         
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Daily
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
     </div>
   );
 }
